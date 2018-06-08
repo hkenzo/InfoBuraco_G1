e9f9e0896e57487ef0a02611219a0138dfefa1c0
Daniel <Danieldiasfl@hotmail.com>
2018-06-08 18:41:11 -0300

    sexta

--- Project1/buraco.cpp
+++ Project1/buraco.cpp
@@ -5,3 +5,25 @@
 buraco::buraco()
 {
 }
+
+void criarBuraco(string nomRua, int nomRua, string posRua, int tamanho, string regional, string tipoCom, string Com, string nomCid, string dataCont, string horaCont) {
+	this->nomRua = nomRua;
+	this->numRua = numRua;
+	this->posicao = posicao;
+	this->tamanho = tamanho;
+	this->regional = regional;
+	
+	this->tipoCom = tipoCom;
+	this->Com = Com;
+	this->nomCid = nomCid;
+	this->dataCont = dataCont;
+	this->horaCont = horaCont;
+
+	this->numReclamacoes = 1;
+	this->statusBuraco = 0; //registrado e não finalizado; 1 = finalizado;
+	this->prioridade = tamanho//calculo com base nos dados informados
+		numBuraco = 3//numero dado ou pelo banco de dados (automatico) ou por um contador no programa;
+
+		criarBuracoDAO(string nomRua, int nomRua, string posRua, int tamanho, string regional, string tipoCom, string Com, string nomCid, string dataCont, string horaCont, int numReclamacoes, int statusBuraco, int prioridade, int numBuraco);
+
+};
--- Project1/buraco.h
+++ Project1/buraco.h
@@ -1,8 +1,31 @@
+#include <stdio.h>
+#include <string>
+
 #pragma once
 ref class buraco
 {
 public:
+	int numBuraco;
+	int prioridade;
+	int numReclamacoes;
+	int statusBuraco;
+
+	string reclamacao;
+
+	string nomRua;
+	int numRua;
+	string posRua;
+	int tamanho;
+	string regional;
+	string tipoCom;
+	string Com;
+	string nomCid;
+	string dataCont;
+	string horaCont;
 
 	buraco();
-};
 
+
+	void criarBuraco(string nomRua, int nomRua, string posRua, int tamanho, string regional, string tipoCom, string Com, string nomCid, string dataCont, string horaCont);
+
+};
