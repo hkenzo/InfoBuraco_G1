0262b5a2cc8af7c5aebe8178f5669af07c13a61a
Daniel <Danieldiasfl@hotmail.com>
2018-06-30 01:06:37 -0300

    aaaaaaaaa

--- Project1/buracoDAO.cpp
+++ Project1/buracoDAO.cpp
@@ -1,8 +1,32 @@
+
+
 #include "buracoDAO.h"
 
-
-buracoDAO::buracoDAO(int numBuraco, string nomeRua, int numeroRua, int tamanho, string posicao, string regional, int prioridade, int numReclamacoes, int satusBuraco, string nomeCidadao, string canalCidadao, string dadoCanal, string dataHora, string reclamacao)
+buracoDAO::buracoDAO()
 {
+
+}
+
+buracoDAO::buracoDAO(int numBuraco, string nomeRua, int numeroRua, int tamanho, string posicao, string regional, int prioridade, int numReclamacoes, int statusBuraco, string nomeCidadao, string canalCidadao, string dadoCanal, string dataHora, string reclamacao)
+{
+	this->nomeRua = nomeRua;
+	this->numeroRua = numeroRua;
+	this->posicao = posicao;
+	this->tamanho = tamanho;
+	this->regional = regional;
+
+	this->canalCidadao = canalCidadao;
+	this->dadoCanal = dadoCanal;
+	this->nomeCidadao = nomeCidadao;
+	this->dataHora = dataHora;
+	this->reclamacao = reclamacao;
+
+	this->numReclamacoes = 1;
+	this->statusBuraco = 0; //registrado e não finalizado; 1 = finalizado;
+	this->prioridade = tamanho;//calculo com base nos dados informadosnumBuraco = 3//numero dado ou pelo banco de dados (automatico) ou por um contador no programa;
+
+	this->numBuraco = 0;
+
 	string log;
 	sql::Connection * connection;
 	sql::Statement* statement;
@@ -41,39 +65,86 @@ buracoDAO::buracoDAO(int numBuraco, string nomeRua, int numeroRua, int tamanho, 
 		connection->close();
 		log = e.what();
 	}
+	connection->close();
 };
 
-int buracoDAO::buscarRua(string nomRua, int numRua)
+int buracoDAO::buscarRua(string nomeRua, int numeroRua)
 {
 	string log;
 	string nomRuaa;
 	int numRuaa;
-	int temp;
+	int temp1;
+	int temp2;
+	int temp3;
 	sql::Connection * connection;
+	sql::Statement* statement;
 	sql::PreparedStatement * preparedStatement;
 	sql::ResultSet *resultSet;
+
 	try {
 		MySQLDAO* mysqldao = MySQLDAO::getInstance();
 		connection = mysqldao->getConnection();
-		preparedStatement = connection->prepareStatement("select numBuraco from buraco where nomeRua = 'fisica'");
+
+		preparedStatement = connection->prepareStatement("SELECT numBuraco, nomeRua, numeroRua, tamanho, posicao, regional, prioridade, numReclamacoes, statusBuraco, nomeCidadao, canalCidadao, dadoCanal, dataHora, reclamacao FROM buraco");
 
 		//preparedStatement->setString(1, nomRua.c_str());
 		//preparedStatement->setInt(2, numRua);
 
 		resultSet = preparedStatement->executeQuery();
-
-		temp = resultSet->getInt(1);
 	}
 	catch (sql::SQLException e)
 	{
 		connection->close();
 		log = e.what();
 	}
-	return temp;
+	temp1 = resultSet->getInt(1);
+	temp3 = resultSet->getInt(2);
+	temp2 = resultSet->getInt(3);
+	resultSet->next();
+	temp1 = resultSet->getInt(1);
+	temp3 = resultSet->getInt(2);
+	temp2 = resultSet->getInt(3);
+	int x = temp1;
+	return temp1;
+	connection->close();
 }
 
 
 
+
+
+
+
+
+
+vector<buracos*>* buracoDAO::buscarRuae()//(int numBuraco, string nomeRua, int numeroRua, int tamanho, string posicao, string regional, int prioridade, int numReclamacoes, int statusBuraco, string nomeCidadao, string canalCidadao, string dadoCanal, string dataHora, string reclamacao)
+{
+	string log;
+	buracos * temp = nullptr;
+	vector<buracos*> *temp2 = nullptr;
+	sql::Connection * connection;
+	sql::PreparedStatement * preparedStatement;
+	sql::ResultSet *resultSet;
+	try {
+		MySQLDAO* mysqldao = MySQLDAO::getInstance();
+		connection = mysqldao->getConnection();
+		preparedStatement = connection->prepareStatement("SELECT numBuraco, nomeRua, numeroRua, tamanho, posicao, regional, prioridade, numReclamacoes, statusBuraco, nomeCidadao, canalCidadao, dadoCanal, dataHora, reclamacao FROM buraco");
+
+		resultSet = preparedStatement->executeQuery();
+
+		temp2 = new vector<buracos*>();
+		while (resultSet->next()) {
+			temp = new buracos(resultSet->getInt(1), resultSet->getString(2), resultSet->getInt(3), resultSet->getInt(4), resultSet->getString(5), resultSet->getString(6), resultSet->getInt(7), resultSet->getInt(8), resultSet->getInt(9), resultSet->getString(10), resultSet->getString(11), resultSet->getString(12), resultSet->getString(13), resultSet->getString(14));
+			temp2->push_back(temp);
+		}
+	}
+	catch (sql::SQLException e)
+	{
+		connection->close();
+		log = e.what();
+	}
+	return temp2;
+}
 
 //buracoDAO::string getString(string campo)
 //{
