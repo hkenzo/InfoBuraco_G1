9f22e04ec1472d04dc7e6afa5b088adfb5eef21e
Kenzo Koga <heitorkenzo.k@gmail.com>
2018-06-30 01:08:49 -0300

    tipo funcionario

--- Project1/CadastroFuncionario.h
+++ Project1/CadastroFuncionario.h
@@ -34,9 +34,11 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  senhabox;
+	protected:
 
 	protected:
-	private: System::Windows::Forms::TextBox^  textBox1;
+
 	private: System::Windows::Forms::Label^  Usuario;
 	private: System::Windows::Forms::Label^  Senha;
 
@@ -48,9 +50,12 @@ namespace Project1 {
 
 
 	private: System::Windows::Forms::Button^  conf_cadastro_bt;
+	private: System::Windows::Forms::TextBox^  userbox;
 
-	private: System::Windows::Forms::TextBox^  textBox3;
+
 	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
 
 	private:
 		/// <summary>
@@ -65,29 +70,31 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->senhabox = (gcnew System::Windows::Forms::TextBox());
 			this->Usuario = (gcnew System::Windows::Forms::Label());
 			this->Senha = (gcnew System::Windows::Forms::Label());
 			this->cancel_cadastro_bt = (gcnew System::Windows::Forms::Button());
 			this->conf_cadastro_bt = (gcnew System::Windows::Forms::Button());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->userbox = (gcnew System::Windows::Forms::TextBox());
 			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
 			this->SuspendLayout();
 			// 
-			// textBox1
+			// senhabox
 			// 
-			this->textBox1->Location = System::Drawing::Point(149, 140);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(215, 20);
-			this->textBox1->TabIndex = 6;
+			this->senhabox->Location = System::Drawing::Point(199, 269);
+			this->senhabox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->senhabox->Name = L"senhabox";
+			this->senhabox->Size = System::Drawing::Size(285, 22);
+			this->senhabox->TabIndex = 6;
 			// 
 			// Usuario
 			// 
 			this->Usuario->AutoSize = true;
-			this->Usuario->Location = System::Drawing::Point(31, 68);
-			this->Usuario->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->Usuario->Location = System::Drawing::Point(41, 105);
 			this->Usuario->Name = L"Usuario";
-			this->Usuario->Size = System::Drawing::Size(43, 13);
+			this->Usuario->Size = System::Drawing::Size(57, 17);
 			this->Usuario->TabIndex = 7;
 			this->Usuario->Text = L"Usuario";
 			this->Usuario->Click += gcnew System::EventHandler(this, &CadastroFuncionario::label1_Click);
@@ -95,10 +102,9 @@ namespace Project1 {
 			// Senha
 			// 
 			this->Senha->AutoSize = true;
-			this->Senha->Location = System::Drawing::Point(31, 142);
-			this->Senha->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->Senha->Location = System::Drawing::Point(41, 272);
 			this->Senha->Name = L"Senha";
-			this->Senha->Size = System::Drawing::Size(38, 13);
+			this->Senha->Size = System::Drawing::Size(49, 17);
 			this->Senha->TabIndex = 9;
 			this->Senha->Text = L"Senha";
 			// 
@@ -107,10 +113,10 @@ namespace Project1 {
 			this->cancel_cadastro_bt->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
 				static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)));
 			this->cancel_cadastro_bt->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
-			this->cancel_cadastro_bt->Location = System::Drawing::Point(50, 309);
-			this->cancel_cadastro_bt->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->cancel_cadastro_bt->Location = System::Drawing::Point(67, 380);
+			this->cancel_cadastro_bt->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->cancel_cadastro_bt->Name = L"cancel_cadastro_bt";
-			this->cancel_cadastro_bt->Size = System::Drawing::Size(128, 29);
+			this->cancel_cadastro_bt->Size = System::Drawing::Size(171, 36);
 			this->cancel_cadastro_bt->TabIndex = 13;
 			this->cancel_cadastro_bt->Text = L"Cancelar";
 			this->cancel_cadastro_bt->UseVisualStyleBackColor = false;
@@ -120,48 +126,71 @@ namespace Project1 {
 			// 
 			this->conf_cadastro_bt->BackColor = System::Drawing::Color::PowderBlue;
 			this->conf_cadastro_bt->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
-			this->conf_cadastro_bt->Location = System::Drawing::Point(223, 309);
-			this->conf_cadastro_bt->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->conf_cadastro_bt->Location = System::Drawing::Point(297, 380);
+			this->conf_cadastro_bt->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->conf_cadastro_bt->Name = L"conf_cadastro_bt";
-			this->conf_cadastro_bt->Size = System::Drawing::Size(128, 29);
+			this->conf_cadastro_bt->Size = System::Drawing::Size(171, 36);
 			this->conf_cadastro_bt->TabIndex = 14;
 			this->conf_cadastro_bt->Text = L"Confirmar";
 			this->conf_cadastro_bt->UseVisualStyleBackColor = false;
 			this->conf_cadastro_bt->Click += gcnew System::EventHandler(this, &CadastroFuncionario::conf_cadastro_bt_Click);
 			// 
-			// textBox3
+			// userbox
 			// 
-			this->textBox3->Location = System::Drawing::Point(149, 66);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(215, 20);
-			this->textBox3->TabIndex = 15;
+			this->userbox->Location = System::Drawing::Point(199, 102);
+			this->userbox->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->userbox->Name = L"userbox";
+			this->userbox->Size = System::Drawing::Size(285, 22);
+			this->userbox->TabIndex = 15;
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(124, 24);
-			this->label5->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->label5->Location = System::Drawing::Point(165, 30);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(195, 20);
+			this->label5->Size = System::Drawing::Size(226, 25);
 			this->label5->TabIndex = 16;
-			this->label5->Text = L"Cadastro de Equipamento";
+			this->label5->Text = L"Cadastro de Funcionário";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(41, 186);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(36, 17);
+			this->label1->TabIndex = 18;
+			this->label1->Text = L"Tipo";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) {
+				L"Serviço de Comunicação", L"Gestor", L"Despachador",
+					L"Chefe da Equipe"
+			});
+			this->comboBox1->Location = System::Drawing::Point(199, 186);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(285, 24);
+			this->comboBox1->TabIndex = 19;
 			// 
 			// CadastroFuncionario
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::Control;
-			this->ClientSize = System::Drawing::Size(410, 372);
+			this->ClientSize = System::Drawing::Size(547, 458);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->label5);
-			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->userbox);
 			this->Controls->Add(this->conf_cadastro_bt);
 			this->Controls->Add(this->cancel_cadastro_bt);
 			this->Controls->Add(this->Senha);
 			this->Controls->Add(this->Usuario);
-			this->Controls->Add(this->textBox1);
-			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
+			this->Controls->Add(this->senhabox);
+			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
 			this->Name = L"CadastroFuncionario";
 			this->Text = L"CadastroFuncionario";
 			this->Load += gcnew System::EventHandler(this, &CadastroFuncionario::CadastroFuncionario_Load);
